79ade38041311d70e570eaae4c6a3136f9a44e1d
Babouchot <bastien.maureille@hotmail.fr>
2018-01-11 21:33:14 +0100

    moved travis_wait call because it is not in the environment of bash scripts

--- ci-before_install.sh
+++ ci-before_install.sh
@@ -3,9 +3,9 @@
 echo "Before install path : $PWD"
 cd ../
 echo "downloading cocos 3.16 ..."
-travis_wait wget --quiet http://cdn.cocos2d-x.org/cocos2d-x-3.16.zip
+wget --quiet http://cdn.cocos2d-x.org/cocos2d-x-3.16.zip
 echo "unzipping cocos 3.16"
-travis_wait unzip -q cocos2d-x-3.16.zip
+unzip -q cocos2d-x-3.16.zip
 export COCOS=$PWD/cocos2d-x-3.16/tools/cocos2d-console/bin/cocos
 echo "creating LudoMuse cocos project"
 $COCOS new LudoMuse -l cpp -p com.IHMTEK.LudoMuse
