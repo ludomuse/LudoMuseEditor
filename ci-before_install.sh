0b84a5e4ed3113fd0f10fe77007e3a1d6cf8fe0b
Bastien Maureille <bastien.maureille@hotmail.fr>
2018-01-11 18:13:02 +0100

    added messages for travis

--- ci-before_install.sh
+++ ci-before_install.sh
@@ -2,8 +2,12 @@
 
 echo "Before install path : $PWD"
 cd ../
+echo "downloading cocos 3.16 ..."
 travis_wait wget --quiet http://cdn.cocos2d-x.org/cocos2d-x-3.16.zip
+echo "unzipping cocos 3.16"
 travis_wait unzip -q cocos2d-x-3.16.zip
 export COCOS=$PWD/cocos2d-x-3.16/tools/cocos2d-console/bin/cocos
+echo "creating LudoMuse cocos project"
 $COCOS new LudoMuse -l cpp com.IHMTEK.LudoMuse
 cd LudoMuseEditor
+
