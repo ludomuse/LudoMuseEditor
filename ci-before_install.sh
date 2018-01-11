5acefb50316df06c7d597833352b6219ad33bc88
Bastien Maureille <bastien.maureille@hotmail.fr>
2018-01-11 18:08:27 +0100

    added travis wait to avoid being kicked by timeout on downloads

--- ci-before_install.sh
+++ ci-before_install.sh
@@ -2,8 +2,8 @@
 
 echo "Before install path : $PWD"
 cd ../
-wget --quiet http://cdn.cocos2d-x.org/cocos2d-x-3.16.zip
-unzip -q cocos2d-x-3.16.zip
+travis_wait wget --quiet http://cdn.cocos2d-x.org/cocos2d-x-3.16.zip
+travis_wait unzip -q cocos2d-x-3.16.zip
 export COCOS=$PWD/cocos2d-x-3.16/tools/cocos2d-console/bin/cocos
 $COCOS new LudoMuse -l cpp com.IHMTEK.LudoMuse
 cd LudoMuseEditor
