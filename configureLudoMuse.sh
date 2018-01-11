014e804247cf616fb78cf6feb46047fee99a8014
Bastien Maureille <bastien.maureille@hotmail.fr>
2018-01-11 18:49:06 +0100

    [ADD] added automation draft script to configure LudoMuse simulator on travis

---
+++ configureLudoMuse.sh
@@ -0,0 +1,10 @@
+cocos new LudoMuse -l cpp -p com.IHMTEK.LudoMuse
+cd LudoMuse/
+git remote add origin https://github.com/ludomuse/Ludomuse.git
+git fetch origin master
+git reset --hard origin/master
+# TODO : weird hack with libbullet
+cocos compile -p linux
+bash changeExeRPATH.sh
+mkdir bin/debug/linux/lib
+mv bin/debug/linux/libfmod.so.6 bin/debug/linux/lib
