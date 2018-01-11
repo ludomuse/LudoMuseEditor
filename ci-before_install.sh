1462efbd5faa6b887a8a58451dea5eec86ec076f
Bastien Maureille <bastien.maureille@hotmail.fr>
2018-01-11 17:58:37 +0100

    made wget and unzip quiet

--- ci-before_install.sh
+++ ci-before_install.sh
@@ -2,8 +2,8 @@
 
 echo "Before install path : $PWD"
 cd ../
-wget http://cdn.cocos2d-x.org/cocos2d-x-3.16.zip
-unzip cocos2d-x-3.16.zip
+wget --quiet http://cdn.cocos2d-x.org/cocos2d-x-3.16.zip
+unzip -q cocos2d-x-3.16.zip
 export COCOS=$PWD/cocos2d-x-3.16/tools/cocos2d-console/bin/cocos
 $COCOS new LudoMuse -l cpp com.IHMTEK.LudoMuse
 cd LudoMuseEditor
