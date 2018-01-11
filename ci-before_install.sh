0dfa83a6a25d1c0024a10eb8e6042d9216867c6a
Bastien Maureille <bastien.maureille@hotmail.fr>
2018-01-11 18:24:35 +0100

    fixed cocos command in before_install script

--- ci-before_install.sh
+++ ci-before_install.sh
@@ -8,6 +8,6 @@ echo "unzipping cocos 3.16"
 travis_wait unzip -q cocos2d-x-3.16.zip
 export COCOS=$PWD/cocos2d-x-3.16/tools/cocos2d-console/bin/cocos
 echo "creating LudoMuse cocos project"
-$COCOS new LudoMuse -l cpp com.IHMTEK.LudoMuse
+$COCOS new LudoMuse -l cpp -p com.IHMTEK.LudoMuse
 cd LudoMuseEditor
 
