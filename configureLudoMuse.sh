07aeea0e325c9f0a371eda15e46b787ca8983486
Babouchot <bastien.maureille@hotmail.fr>
2018-01-12 03:47:11 +0100

    travis config should now be complete

--- configureLudoMuse.sh
+++ configureLudoMuse.sh
@@ -1,11 +1,11 @@
-cocos new LudoMuse -l cpp -p com.IHMTEK.LudoMuse
 cd LudoMuse/
 git init
 git remote add origin https://github.com/ludomuse/Ludomuse.git
 git fetch origin master
 git reset --hard origin/master
 # TODO : weird hack with libbullet
+./doBulletHack.sh
 cocos compile -p linux
-bash changeExeRPATH.sh
+./changeExeRPATH.sh
 mkdir bin/debug/linux/lib
-mv bin/debug/linux/libfmod.so.6 bin/debug/linux/lib
+cp prebuilt/libfmod.so.6 bin/debug/linux/lib
