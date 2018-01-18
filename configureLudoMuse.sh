399e097c665682ac4db06c791e6a18fe6bfc6334
Bastien Maureille <bastien.maureille@hotmail.fr>
2018-01-18 17:44:35 +0100

    [TRAVIS] removed libbullet hack from travis configuration

--- configureLudoMuse.sh
+++ configureLudoMuse.sh
@@ -4,6 +4,6 @@ git remote add origin https://github.com/ludomuse/Ludomuse.git
 git fetch origin master
 git reset --hard origin/master
 # TODO : weird hack with libbullet
-./doBulletHack.sh
+# ./doBulletHack.sh
 $1 compile -p linux
 ./changeExeRPATH.sh
