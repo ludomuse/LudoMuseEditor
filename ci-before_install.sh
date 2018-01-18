8e0387f5b964488442abb3fbd87cac68ba151b0b
Bastien Maureille <bastien.maureille@hotmail.fr>
2018-01-18 16:48:45 +0100

    [TRAVIS] glfw version

--- ci-before_install.sh
+++ ci-before_install.sh
@@ -8,7 +8,7 @@ wget --quiet http://cdn.cocos2d-x.org/cocos2d-x-3.16.zip
 echo "unzipping cocos 3.16"
 unzip -q $PREF/cocos2d-x-3.16.zip -d $PREF
 export COCOS=$PREF/cocos2d-x-3.16/tools/cocos2d-console/bin/cocos
-cocos2d-x-3.16/tools/travis-scripts/install_glfw.sh
+$PREF/LudoMuseEditor/install_glfw.sh
 echo "creating LudoMuse cocos project"
 stat $COCOS
 yes N | $COCOS new LudoMuse -l cpp -p com.IHMTEK.LudoMuse
