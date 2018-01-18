ffaecf04c8da8743cc0856c58babd6d79a9e812d
Babouchot <bastien.maureille@hotmail.fr>
2018-01-18 12:04:10 +0100

    added mac prebuilt libraries

---
+++ libs/zlib1211/mac_build/CMakeFiles/zlibstatic.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/adler32.c" "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/mac_build/CMakeFiles/zlibstatic.dir/adler32.o"
+  "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/compress.c" "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/mac_build/CMakeFiles/zlibstatic.dir/compress.o"
+  "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/crc32.c" "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/mac_build/CMakeFiles/zlibstatic.dir/crc32.o"
+  "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/deflate.c" "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/mac_build/CMakeFiles/zlibstatic.dir/deflate.o"
+  "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/gzclose.c" "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/mac_build/CMakeFiles/zlibstatic.dir/gzclose.o"
+  "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/gzlib.c" "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/mac_build/CMakeFiles/zlibstatic.dir/gzlib.o"
+  "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/gzread.c" "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/mac_build/CMakeFiles/zlibstatic.dir/gzread.o"
+  "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/gzwrite.c" "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/mac_build/CMakeFiles/zlibstatic.dir/gzwrite.o"
+  "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/infback.c" "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/mac_build/CMakeFiles/zlibstatic.dir/infback.o"
+  "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/inffast.c" "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/mac_build/CMakeFiles/zlibstatic.dir/inffast.o"
+  "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/inflate.c" "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/mac_build/CMakeFiles/zlibstatic.dir/inflate.o"
+  "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/inftrees.c" "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/mac_build/CMakeFiles/zlibstatic.dir/inftrees.o"
+  "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/trees.c" "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/mac_build/CMakeFiles/zlibstatic.dir/trees.o"
+  "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/uncompr.c" "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/mac_build/CMakeFiles/zlibstatic.dir/uncompr.o"
+  "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/zutil.c" "/Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/mac_build/CMakeFiles/zlibstatic.dir/zutil.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ libs/zlib1211/mac_build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211
+# Build directory: /Users/ihmtek/workspace/LudoMuseEditor/libs/zlib1211/mac_build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
