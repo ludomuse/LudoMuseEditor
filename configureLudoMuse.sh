7c7d5a257e4204e070bbc528b2c8fcc4ec5a283b
Babouchot <bastien.maureille@hotmail.fr>
2018-01-19 16:17:39 +0100

    [TRAVIS] removing bullet dependency from the linux editor build

--- configureLudoMuse.sh
+++ configureLudoMuse.sh
@@ -3,7 +3,10 @@ git init
 git remote add origin https://github.com/ludomuse/Ludomuse.git
 git fetch origin master
 git reset --hard origin/master
-# TODO : weird hack with libbullet
+# weird hack with libbullet
 # ./doBulletHack.sh
+mkdir -p linux-build
+cd linux-build
+cmake ../ -DUSE_BULLET=0
 $1 compile -p linux
 ./changeExeRPATH.sh
