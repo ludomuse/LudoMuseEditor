4067044fd7814885b96be1d1f5a1d2ee71efccf9
Bastien Maureille <bastien.maureille@hotmail.fr>
2018-01-22 15:53:30 +0100

    [APPVEYOR] fixed path generation

--- configure-cocos-win.cmd
+++ configure-cocos-win.cmd
@@ -4,9 +4,10 @@ echo "downloading cocos 3.16 ..."
 curl -s -O http://cdn.cocos2d-x.org/cocos2d-x-3.16.zip
 echo "unzipping cocos 3.16"
 unzip -q %PREF%/cocos2d-x-3.16.zip -d %PREF%
-set COCOS=%PREF%/cocos2d-x-3.16/tools/cocos2d-console/bin/cocos
+set COCOS="%PREF%/cocos2d-x-3.16/tools/cocos2d-console/bin/cocos"
 rem %PREF%/LudoMuseEditor/install_glfw.cmd
 echo "creating LudoMuse cocos project"
 echo N | %COCOS% new LudoMuse -l cpp -p com.IHMTEK.LudoMuse
-call %PREF%/LudoMuseEditor/configure-LudoMuse-win.cmd $COCOS
+echo "configuring LudoMuse as editor dependency"
+call "%PREF%/LudoMuseEditor/configure-LudoMuse-win.cmd" $COCOS
 cd $PREF/LudoMuseEditor
