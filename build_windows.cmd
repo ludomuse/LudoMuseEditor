e78134a19f4d42972bdcd47ae02143d61e551e76
Bastien Maureille <bastien.maureille@hotmail.fr>
2018-01-22 12:00:10 +0100

    [APPVEYOR] attempt to automate builds on appveyor

--- build_windows.cmd
+++ build_windows.cmd
@@ -7,31 +7,34 @@ set PATH=%QT%\bin\;C:\Qt\Tools\QtCreator\bin\;C:\Qt\QtIFW2.0.1\bin\;%PATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %PLATFORM%
 
 echo Building LudoMuseEditor...
-qmake -spec win32-msvc2015 CONFIG+=x86_64 CONFIG-=debug CONFIG+=release
+cd ../
+mkdir LudoMuseEditorWin
+cd LudoMuseEditorWin
+qmake -spec win32-msvc2015 CONFIG+=x86_64 CONFIG-=debug CONFIG+=release ../LudoMuseEditor
 nmake
 
 echo Running tests...
 
 echo Packaging...
 cd %project_dir%\build\windows\msvc\x86_64\release\
-windeployqt --qmldir ..\..\..\..\..\src\ LudoMuseEditor\LudoMuseEditor.exe
+windeployqt LudoMuseEditor.exe
 
-rd /s /q LudoMuseEditor\moc\
-rd /s /q LudoMuseEditor\obj\
-rd /s /q LudoMuseEditor\qrc\
+rem rd /s /q LudoMuseEditor\moc\
+rem rd /s /q LudoMuseEditor\obj\
+rem rd /s /q LudoMuseEditor\qrc\
 
-echo Copying project files for archival...
-copy "%project_dir%\README.md" "LudoMuseEditor\README.md"
-copy "%project_dir%\LICENSE" "LudoMuseEditor\LICENSE.txt"
-copy "%project_dir%\Qt License" "LudoMuseEditor\Qt License.txt"
+rem echo Copying project files for archival...
+rem copy "%project_dir%\README.md" "LudoMuseEditor\README.md"
+rem copy "%project_dir%\LICENSE" "LudoMuseEditor\LICENSE.txt"
+rem copy "%project_dir%\Qt License" "LudoMuseEditor\Qt License.txt"
 
-echo Copying files for installer...
-mkdir "%project_dir%\installer\windows\x86_64\packages\com.yourappproject.yourapp\data\"
-robocopy LudoMuseEditor\ "%project_dir%\installer\windows\x86_64\packages\com.yourappproject.yourapp\data" /E
+rem echo Copying files for installer...
+rem mkdir "%project_dir%\installer\windows\x86_64\packages\com.yourappproject.yourapp\data\"
+rem robocopy LudoMuseEditor\ "%project_dir%\installer\windows\x86_64\packages\com.yourappproject.yourapp\data" /E
 
-echo Packaging portable archive...
-7z a LudoMuseEditor_%TAG_NAME%_windows_x86_64_portable.zip LudoMuseEditor
+rem echo Packaging portable archive...
+rem 7z a LudoMuseEditor_%TAG_NAME%_windows_x86_64_portable.zip LudoMuseEditor
 
-echo Creating installer...
-cd %project_dir%\installer\windows\x86_64\
-binarycreator.exe --offline-only -c config\config.xml -p packages LudoMuseEditor_%TAG_NAME%_windows_x86_64_installer.exe
+rem echo Creating installer...
+rem cd %project_dir%\installer\windows\x86_64\
+rem binarycreator.exe --offline-only -c config\config.xml -p packages LudoMuseEditor_%TAG_NAME%_windows_x86_64_installer.exe
