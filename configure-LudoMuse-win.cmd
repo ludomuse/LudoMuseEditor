7d5288c8b6533f0c955ab11e62ad3eafdad93635
Babouchot <bastien.maureille@hotmail.fr>
2018-01-23 01:05:12 +0100

    [APPVEYOR] replace cocos compile with cmake + nmake

--- configure-LudoMuse-win.cmd
+++ configure-LudoMuse-win.cmd
@@ -3,4 +3,8 @@ git init
 git remote add origin https://github.com/ludomuse/Ludomuse.git
 git fetch origin master
 git reset --hard origin/master
-%1 compile -p win32 -m release
+rem %1 compile -p win32 -m release
+mkdir win-build
+cd win-build
+cmake -G"NMake Makefiles" -DUSE_BULLET=0 -DUSE_RECAST=0 -DUSE_CHIPMUNK=0 -DCMAKE_CXX_FLAGS="-I %cd%/../prebuilt/win/include" ../
+nmake
