62fd832b3e4ec853e38bdf1c6639d7fe477b9da9
Bastien Maureille <bastien.maureille@hotmail.fr>
2018-01-24 11:20:09 +0100

    [APPVEYOR] use cocos compile instead of nmake now that correct project files are uploaded in LudoMuse

--- configure-LudoMuse-win.cmd
+++ configure-LudoMuse-win.cmd
@@ -3,8 +3,8 @@ git init
 git remote add origin https://github.com/ludomuse/Ludomuse.git
 git fetch origin master
 git reset --hard origin/master
-rem %1 compile -p win32 -m release
-mkdir win-build
-cd win-build
-cmake -G"NMake Makefiles" -DUSE_BULLET=0 -DUSE_RECAST=0 -DUSE_CHIPMUNK=0 -DCMAKE_CXX_FLAGS="-I %cd%/../prebuilt/win/include" ../
-nmake
+%1 compile -p win32 -m release
+rem mkdir win-build
+rem cd win-build
+rem cmake -G"NMake Makefiles" -DUSE_BULLET=0 -DUSE_RECAST=0 -DUSE_CHIPMUNK=0 -DCMAKE_CXX_FLAGS="-I %cd%/../prebuilt/win/include" ../
+rem nmake
