046c64d2c81a3e764a91bbe2c606304ea1800078
Bastien Maureille <bastien.maureille@hotmail.fr>
2018-01-25 17:16:40 +0100

    [TRAVIS] removed hard links to g++-7 prefering using CXX env variable

--- build_linux.sh
+++ build_linux.sh
@@ -2,14 +2,14 @@
 #sudo rm $GCC_PATH
 #sudo ln -s $(dirname GCC_PATH)/g++-7 $GCC_PATH
 echo "Build will use g++ version :"
-/usr/bin/g++-7 --version
+$CXX --version
 cd libs/quazip/linux_build
 rm *
 /opt/qt58/bin/qmake ../quazip.pro CONFIG-=debug CONFIG+=release
-make CXX=/usr/bin/g++7
+make
 cd ../../../../
 mkdir LudoMuseEditorLinux
 cd LudoMuseEditorLinux
 /opt/qt58/bin/qmake --version
 /opt/qt58/bin/qmake ../LudoMuseEditor/LudoMuseEditor.pro
-make CXX=/usr/bin/g++-7
+make
