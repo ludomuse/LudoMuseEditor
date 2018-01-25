d79825b2a7eff959474067d5e33c6a64165bf2ef
Bastien Maureille <bastien.maureille@hotmail.fr>
2018-01-25 16:26:05 +0100

    [TRAVIS] typo in g++ version

--- build_linux.sh
+++ build_linux.sh
@@ -2,7 +2,7 @@
 #sudo rm $GCC_PATH
 #sudo ln -s $(dirname GCC_PATH)/g++-7 $GCC_PATH
 echo "Build will use g++ version :"
-/usr/bin/g++7 --version
+/usr/bin/g++-7 --version
 cd libs/quazip/linux_build
 rm *
 /opt/qt58/bin/qmake ../quazip.pro CONFIG-=debug CONFIG+=release
@@ -12,4 +12,4 @@ mkdir LudoMuseEditorLinux
 cd LudoMuseEditorLinux
 /opt/qt58/bin/qmake --version
 /opt/qt58/bin/qmake ../LudoMuseEditor/LudoMuseEditor.pro
-make CXX=/usr/bin/g++7
+make CXX=/usr/bin/g++-7
