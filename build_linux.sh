3f19d1091e15ea89bc3d75c2700a962ab8090980
Bastien Maureille <bastien.maureille@hotmail.fr>
2018-01-25 15:13:00 +0100

    [TRAVIS] changed g++7 handling

--- build_linux.sh
+++ build_linux.sh
@@ -1,12 +1,12 @@
-export GCC_PATH=$(which g++)
-sudo rm $GCC_PATH
-sudo ln -s $(dirname GCC_PATH)/g++-7 $GCC_PATH
+#export GCC_PATH=$(which g++)
+#sudo rm $GCC_PATH
+#sudo ln -s $(dirname GCC_PATH)/g++-7 $GCC_PATH
 echo "Build will use g++ version :"
-/usr/bin/g++ --version
+/usr/bin/g++7 --version
 cd libs/quazip/linux_build
 rm *
 /opt/qt58/bin/qmake ../quazip.pro CONFIG-=debug CONFIG+=release
-make
+make CXX=/usr/bin/g++7
 cd ../../../../
 mkdir LudoMuseEditorLinux
 cd LudoMuseEditorLinux
