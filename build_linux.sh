f7918edbcb36ab9671231dce1ca623d7ca5904e3
Bastien Maureille <bastien.maureille@hotmail.fr>
2018-01-29 14:40:40 +0100

    [TRAVIS] reduced the number of build jobs

--- build_linux.sh
+++ build_linux.sh
@@ -6,10 +6,10 @@ $CXX --version
 cd libs/quazip/linux_build
 rm *
 /opt/qt58/bin/qmake ../quazip.pro CONFIG-=debug CONFIG+=release
-make
+make -j1
 cd ../../../../
 mkdir $HOME/LudoMuseEditor_build
 cd $HOME/LudoMuseEditor_build
 /opt/qt58/bin/qmake --version
 /opt/qt58/bin/qmake CONFIG+=x86_64 CONFIG-=debug CONFIG+=release $TRAVIS_BUILD_DIR/LudoMuseEditor.pro
-make
+make -j1
