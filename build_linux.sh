8922e8fd28bb1d92ac4dbeed773571e34dc21bd2
Bastien Maureille <bastien.maureille@hotmail.fr>
2018-01-29 15:14:59 +0100

    [FIX] fixed g++ version used

--- build_linux.sh
+++ build_linux.sh
@@ -2,14 +2,15 @@
 #sudo rm $GCC_PATH
 #sudo ln -s $(dirname GCC_PATH)/g++-7 $GCC_PATH
 echo "Build will use g++ version :"
+export CXX=CXX=/usr/bin/g++-7
 $CXX --version
 cd libs/quazip/linux_build
 rm *
 /opt/qt58/bin/qmake ../quazip.pro CONFIG-=debug CONFIG+=release
-make -j1
+make CXX=/usr/bin/g++-7
 cd ../../../../
 mkdir $HOME/LudoMuseEditor_build
 cd $HOME/LudoMuseEditor_build
 /opt/qt58/bin/qmake --version
 /opt/qt58/bin/qmake CONFIG+=x86_64 CONFIG-=debug CONFIG+=release $TRAVIS_BUILD_DIR/LudoMuseEditor.pro
-make -j1
+make CXX=/usr/bin/g++-7
