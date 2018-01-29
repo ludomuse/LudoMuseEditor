db95b55d2bbdb8b2418f84858cf2f646d1e6e848
Babouchot <bastien.maureille@hotmail.fr>
2018-01-29 21:16:38 +0100

    [TRAVIS] CI tweaks

--- build_linux.sh
+++ build_linux.sh
@@ -1,6 +1,9 @@
 #export GCC_PATH=$(which g++)
 #sudo rm $GCC_PATH
 #sudo ln -s $(dirname GCC_PATH)/g++-7 $GCC_PATH
+echo "startig build"
+echo $TRAVIS_BUILD_DIR
+#export TRAVIS_BUILD_DIR=/home/travis/build/ludomuse/LudoMuseEditor
 echo "Build will use g++ version :"
 export CXX=/usr/bin/g++-7
 $CXX --version
@@ -12,5 +15,5 @@ cd ../../../../
 mkdir $HOME/LudoMuseEditor_build
 cd $HOME/LudoMuseEditor_build
 /opt/qt58/bin/qmake --version
-/opt/qt58/bin/qmake CONFIG+=x86_64 CONFIG-=debug CONFIG+=release LUDOMUSE_PATH=$TRAVIS_BUILD_DIR/../LudoMuse/ $TRAVIS_BUILD_DIR/LudoMuseEditor.pro
+/opt/qt58/bin/qmake CONFIG+=x86_64 CONFIG-=debug CONFIG+=release "LUDOMUSE_PATH=${TRAVIS_BUILD_DIR}/../LudoMuse/" $TRAVIS_BUILD_DIR/LudoMuseEditor.pro
 make CXX=/usr/bin/g++-7
