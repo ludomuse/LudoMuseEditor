67845822f2b15e7855c5b2f11f3e9ca3f77ffaef
Babouchot <bastien.maureille@hotmail.fr>
2018-02-09 20:24:52 +0100

    [TRAVIS] accidentally commented qt dependency

--- before_build_linux.sh
+++ before_build_linux.sh
@@ -1,7 +1,7 @@
 echo -e "machine github.com\n login $GITHUB_TOKEN" >> ~/.netrc
 #git lfs pull
 sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
-#sudo add-apt-repository ppa:beineri/opt-qt58-trusty -y
+sudo add-apt-repository ppa:beineri/opt-qt58-trusty -y
 #echo "deb http://ppa.launchpad.net/keithw/glfw3/ubuntu trusty main" | sudo tee -a /etc/apt/sources.list.d/fillwave_ext.list
 #echo "deb-src http://ppa.launchpad.net/keithw/glfw3/ubuntu trusty main" | sudo tee -a /etc/apt/sources.list.d/fillwave_ext.list
 #- sudo add-apt-repository --yes ppa:pyglfw/pyglfw
