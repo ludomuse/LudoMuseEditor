5e17c5bf589f5edb731d96b0ac4a438e0c3106ec
Bastien Maureille <bastien.maureille@hotmail.fr>
2018-02-12 11:25:24 +0100

    [TRAVIS] build linux and libcocos in debug mode to troubleshoot click issue in the editor

--- configure-LudoMuse-linux.sh
+++ configure-LudoMuse-linux.sh
@@ -9,5 +9,5 @@ mkdir -p linux-build
 cd linux-build
 cmake ../ -DUSE_BULLET=0
 cd ../
-$1 compile -p linux -m release
-./changeExeRPATH.sh release
+$1 compile -p linux #-m release
+./changeExeRPATH.sh #release
